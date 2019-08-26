01f42cf195f70e8d91f07975db5e02744da04379
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-08-26 18:20:16 -0300

    filters properly showing up

--- tasks/filters.py
+++ tasks/filters.py
@@ -1,8 +1,22 @@
 import django_filters
-from .models import Task
+from django import forms
+from .models import Task, Priority, Event
 
 
 class TaskFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        lookup_expr='icontains',
+        label='Name'
+    )
+    priority = django_filters.ModelChoiceFilter(
+        queryset=Priority.objects.all(),
+        widget=forms.Select
+    )
+    event = django_filters.ModelChoiceFilter(
+        queryset=Event.objects.all(),
+        widget=forms.Select
+    )
+
     class Meta:
         model = Task
-        fields = ['name', 'priority__name', 'event__name']
+        fields = ['name', 'priority', 'event']
