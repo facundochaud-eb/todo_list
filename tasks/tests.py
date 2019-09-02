6bc62fa4633bebafe6d36d5feef75da681ed9fff
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-02 12:15:46 -0300

    test event str

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .models import Priority
-# from .factories import TaskFactory
+from .factories import TaskFactory, EventFactory, UserFactory
 from parameterized import parameterized
 
 PRIORITIES = ["LOW", "NORMAL", "URGENT"]
@@ -22,3 +22,9 @@ class TaskModelsTestCase(TestCase):
     def test_priority_str(self, name):
         priority = Priority.objects.create(name=name)
         self.assertEqual(str(priority), name)
+
+    def test_event_str(self):
+        event = EventFactory.create()
+        self.assertIn(str(event.id),   str(event))
+        self.assertIn(str(event.name), str(event))
+        self.assertIn(str(event.user), str(event))
