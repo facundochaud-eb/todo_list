e2247dbebcba09eda243bf06df791b71a6a49b19
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-09-04 12:37:17 -0300

    fix travis with no env variables

--- settings/base.py
+++ settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from settings import get_env_variable
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +73,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'todolistsite.wsgi.application'
-
-
-SOCIAL_AUTH_EVENTBRITE_KEY = get_env_variable('SOCIAL_AUTH_EVENTBRITE_KEY')
-SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable('SOCIAL_AUTH_EVENTBRITE_SECRET')
 
 
 AUTHENTICATION_BACKENDS = (
